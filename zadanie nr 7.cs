efbb4da7c103e62335a954204906ade0fe6dfd14
hubertwi1115 <148622055+hubertwi1115@users.noreply.github.com>
2023-11-04 15:01:34 +0100

    Add files via upload

---
+++ zadanie nr 7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj rok urodzenia:");
+        int rokUrodzenia = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj imię i nazwisko:");
+        string imieNazwisko = Console.ReadLine();
+
+        Console.WriteLine("Podaj wiek:");
+        int wiek = int.Parse(Console.ReadLine());
+        if (wiek < 50)
+        {
+            Console.WriteLine("Wiek jest mniejszy niż 50 lat. Kończę program.");
+            return; // Przerwij wykonanie programu
+        }
+
+       Console.WriteLine("Podaj miejsce urodzenia:");
+        string miejsceUrodzenia = Console.ReadLine();
+
+        Console.WriteLine($"Dane osobowe: {rokUrodzenia}, {imieNazwisko}, {wiek} lat, {miejsceUrodzenia}");
+    }
+}
